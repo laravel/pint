102f59336bdbc20829af638c3dfe4f64c486a2ab
Nuno Maduro <enunomaduro@gmail.com>
2022-06-17 12:30:01 +0100

    Adds pretty comments

--- config/view.php
+++ config/view.php
@@ -1,11 +1,33 @@
 <?php
 
 return [
-    'cache' => false,
+    /*
+    |--------------------------------------------------------------------------
+    | Compiled View Path
+    |--------------------------------------------------------------------------
+    |
+    | This option determines where all the compiled Blade templates will be
+    | stored for your application. Typically, this is within the storage
+    | directory. However, as usual, you are free to change this value.
+    |
+    */
 
+    'cache' => false,
     'compiled' => realpath(sys_get_temp_dir()),
+
+    /*
+    |--------------------------------------------------------------------------
+    | View Storage Paths
+    |--------------------------------------------------------------------------
+    |
+    | Most templating systems load templates from disk. Here you may specify
+    | an array of paths that should be checked for your views. Of course
+    | the usual Laravel view path has already been registered for you.
+    |
+    */
 
     'paths' => [
         resource_path('views'),
     ],
+
 ];
