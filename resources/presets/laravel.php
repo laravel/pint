86dd3a9d30dec2fe4d6b5639b0065c32f156267b
Nuno Maduro <enunomaduro@gmail.com>
2022-06-17 10:26:18 +0100

    Fixes renaming classes by Laravel preset

--- resources/presets/laravel.php
+++ resources/presets/laravel.php
@@ -100,7 +100,7 @@ return ConfigurationFactory::preset([
     'not_operator_with_successor_space' => true,
     'object_operator_without_whitespace' => true,
     'ordered_imports' => ['sort_algorithm' => 'alpha'],
-    'psr_autoloading' => true,
+    'psr_autoloading' => false,
     'phpdoc_indent' => true,
     'phpdoc_inline_tag_normalizer' => true,
     'phpdoc_no_access' => true,
