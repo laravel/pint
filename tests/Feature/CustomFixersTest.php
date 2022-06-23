5bdf1dae7c4860d5727aac74cfc78210f18479da
Luke Downing <lukeraymonddowning@gmail.com>
2022-06-23 14:01:12 +0100

    Adds support for specifying custom rules in `pint.json`

--- tests/Feature/CustomFixersTest.php
+++ tests/Feature/CustomFixersTest.php
@@ -9,6 +9,6 @@ it('runs custom fixers', function () {
         ->and($output->fetch())
         ->toContain('FAIL')
         ->toContain('1 file, 1 style issue')
-        ->toContain('⨯ empty.php')
+        ->toContain('empty.php')
         ->toContain('Laravel/test_custom_fixer');
 });
