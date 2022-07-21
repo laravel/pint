b0c7ccc09ae6a96425fc871926a0886ad1ad6e73
Nuno Maduro <enunomaduro@gmail.com>
2022-07-21 09:03:18 +0100

    Clarifies  test

--- tests/Feature/ConfigurationTest.php
+++ tests/Feature/ConfigurationTest.php
@@ -2,13 +2,8 @@
 
 use LaravelZero\Framework\Exceptions\ConsoleException;
 
-$exceptionMessage = sprintf(
-    'The configuration file [%s/tests/Fixtures/with-invalid-configuration/pint.json] is not valid JSON format.',
-    getcwd(),
-);
-
 it('ensures configuration file is valid', function () {
     [$statusCode, $output] = run('default', [
         'path' => base_path('tests/Fixtures/with-invalid-configuration'),
     ]);
-})->throws(ConsoleException::class, $exceptionMessage);
+})->throws(ConsoleException::class, 'is not valid JSON format.');
