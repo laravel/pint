eaae0684faf5115056cffa979743cd410a2b4bb7
Zep Fietje <hey@zepfietje.com>
2022-11-29 16:15:38 +0100

    Enable types_spaces rule in Laravel preset (#128)

--- resources/presets/laravel.php
+++ resources/presets/laravel.php
@@ -170,6 +170,7 @@ return ConfigurationFactory::preset([
     'ternary_operator_spaces' => true,
     'trailing_comma_in_multiline' => ['elements' => ['arrays']],
     'trim_array_spaces' => true,
+    'types_spaces' => true,
     'unary_operator_spaces' => true,
     'visibility_required' => [
         'elements' => ['method', 'property'],
