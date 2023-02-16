27eeb7bc9ca241287a27f0ec2082a59f6f17dfe1
Simon Rigby <575561+Rigby90@users.noreply.github.com>
2023-02-16 18:43:30 +0000

    Support Windows paths for --dirty

--- app/Repositories/GitPathsRepository.php
+++ app/Repositories/GitPathsRepository.php
@@ -41,7 +41,13 @@ class GitPathsRepository implements PathsRepository
             ->mapWithKeys(fn ($file) => [substr($file, 3) => trim(substr($file, 0, 3))])
             ->reject(fn ($status) => $status === 'D')
             ->map(fn ($status, $file) => $status === 'R' ? Str::after($file, ' -> ') : $file)
-            ->map(fn ($file) => $this->path.DIRECTORY_SEPARATOR.$file)
+            ->map(function ($file) {
+                if (PHP_OS_FAMILY === 'Windows') {
+                    $file = str_replace('/', DIRECTORY_SEPARATOR, $file);
+                }
+
+                return $this->path.DIRECTORY_SEPARATOR.$file;
+            })
             ->values()
             ->all();
 
