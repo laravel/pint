cc904c01ed1850e954b1c0f7891e7a209e434517
Len Woodward <len@projektgopher.com>
2025-01-06 07:20:12 -0800

    [1.x] Adds `--diff` option (#327)
    * add [diff] option to default command
    
    * add path resolver for git diff
    
    * add [diff] method to PathsRepository contract. (Breaking?)
    
    * extract method for future re-use
    
    * implement new [diff] method
    
    * lint
    
    * skip duplicates
    
    * include untracked files in diff list
    
    * add tests
    
    * fixes
    
    * make stan happy

--- app/Commands/DefaultCommand.php
+++ app/Commands/DefaultCommand.php
@@ -41,6 +41,7 @@ class DefaultCommand extends Command
                     new InputOption('test', '', InputOption::VALUE_NONE, 'Test for code style errors without fixing them'),
                     new InputOption('bail', '', InputOption::VALUE_NONE, 'Test for code style errors without fixing them and stop on first error'),
                     new InputOption('repair', '', InputOption::VALUE_NONE, 'Fix code style errors but exit with status 1 if there were any changes made'),
+                    new InputOption('diff', '', InputOption::VALUE_REQUIRED, 'Only fix files that have changed since branching off from the given branch', null, ['main', 'master', 'origin/main', 'origin/master']),
                     new InputOption('dirty', '', InputOption::VALUE_NONE, 'Only fix files that have uncommitted changes'),
                     new InputOption('format', '', InputOption::VALUE_REQUIRED, 'The output format that should be used'),
                     new InputOption('cache-file', '', InputArgument::OPTIONAL, 'The path to the cache file'),
--- app/Contracts/PathsRepository.php
+++ app/Contracts/PathsRepository.php
@@ -10,4 +10,12 @@ interface PathsRepository
      * @return array<int, string>
      */
     public function dirty();
+
+    /**
+     * Determine the files that have changed since branching off from the given branch.
+     *
+     * @param  string  $branch
+     * @return array<int, string>
+     */
+    public function diff($branch);
 }
