6cb5bec2ddf935933db1b97c3452c9305e2e992f
Taylor Otwell <taylor@laravel.com>
2025-03-28 09:14:11 -0700

    Update DefaultCommand.php

--- app/Commands/DefaultCommand.php
+++ app/Commands/DefaultCommand.php
@@ -39,13 +39,13 @@ class DefaultCommand extends Command
                     new InputOption('no-config', '', InputOption::VALUE_NONE, 'Disable loading any configuration file'),
                     new InputOption('preset', '', InputOption::VALUE_REQUIRED, 'The preset that should be used'),
                     new InputOption('test', '', InputOption::VALUE_NONE, 'Test for code style errors without fixing them'),
-                    new InputOption('output-to-file', '', InputOption::VALUE_REQUIRED, 'Output results to file at this path. Requires either --format or --output-format'),
-                    new InputOption('output-format', '', InputOption::VALUE_REQUIRED, 'Output results to file in this output format. Falls back to format defined in --format'),
                     new InputOption('bail', '', InputOption::VALUE_NONE, 'Test for code style errors without fixing them and stop on first error'),
                     new InputOption('repair', '', InputOption::VALUE_NONE, 'Fix code style errors but exit with status 1 if there were any changes made'),
                     new InputOption('diff', '', InputOption::VALUE_REQUIRED, 'Only fix files that have changed since branching off from the given branch', null, ['main', 'master', 'origin/main', 'origin/master']),
                     new InputOption('dirty', '', InputOption::VALUE_NONE, 'Only fix files that have uncommitted changes'),
                     new InputOption('format', '', InputOption::VALUE_REQUIRED, 'The output format that should be used'),
+                    new InputOption('output-to-file', '', InputOption::VALUE_REQUIRED, 'Output the test results to a file at this path'),
+                    new InputOption('output-format', '', InputOption::VALUE_REQUIRED, 'The format that should be used when outputting the test results to a file'),
                     new InputOption('cache-file', '', InputArgument::OPTIONAL, 'The path to the cache file'),
                 ]
             );
